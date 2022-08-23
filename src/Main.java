186bc1efbe75afe1ff7ff0efb7f5509312380978
tKons64 <spb_unex@mail.ru>
2022-08-23 13:24:12 +0300

    Сделано задание №1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,30 @@
 public class Main {
     public static void main(String[] args) {
 
-    var privet = 1;
-    System.out.println(privet);
+// Задание №1
+    byte a = 100;
+    short b = 2000;
+    int c = 3000;
+    long d = 365L;
+
+    float A = 1.5555555555f;
+    double B = 1.5555555555;
+
+    char C = '!';
+    boolean YesNo = true;
+
+    System.out.println("Задание №1:");
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+    System.out.println(d);
+    System.out.println(A);
+    System.out.println(B);
+    System.out.println(C);
+    System.out.println(YesNo);
+
+
+
 
     }
 }
